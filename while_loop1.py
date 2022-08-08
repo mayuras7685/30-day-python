ec21e8bc02c8766c5ec478f198d59fb46d0ee91e
mayuras7685 <mayurasodara@gmail.com>
2022-08-08 21:19:41 +0530

    while_loops

---
+++ while_loop1.py
@@ -0,0 +1,43 @@
+'''
+while (condition):
+  ----while block----
+
+
+'''
+
+i = 0
+
+while(i<10):
+  print(i)
+  i += 1
+
+'''
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+'''
+#else with while
+
+n = 1
+while n < 6:
+  print(n)
+  n += 1
+else:
+  print("i is no longer less than 6")
+"""
+output:
+1
+2
+3
+4
+5
+i is no longer less than 6
+"""
+
