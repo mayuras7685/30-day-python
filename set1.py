322c4841fcca7785c01e3bb32b9041d2e14385eb
mayuras7685 <mayurasodara@gmail.com>
2022-08-11 22:11:00 +0530

    sets

---
+++ set1.py
@@ -0,0 +1,14 @@
+set1 = {"Mayur", "Mohit", "Viraj"}
+print(set1)
+# output: {'Viraj', 'Mohit', 'Mayur'}
+# 2nd time we run this code {'Viraj', 'Mayur', 'Mohit'}
+
+"""
+the items in a set do not have a defined order.
+Set items can appear in a different order every time you use them, and cannot be referred to by index or key.
+"""
+
+# Duplicates not allowed : Sets cannot have two items with the same value.
+set2 = {2, 3, 4, 2, 5, 6, 3}
+print(set2)
+# output: {2, 3, 4, 5, 6}
