04ab762add228ee251e54b1043eb42f24e591b8f
mayuras7685 <mayurasodara@gmail.com>
2022-08-18 20:50:01 +0530

    *args **kwargs

---
+++ args_kw1.py
@@ -0,0 +1,33 @@
+def funargs(normal, *args, **kw):
+  print(normal)
+  for item in args:
+    print(item)
+  print("Every individual role:\n")
+  for key, value in roles.items():
+    print(f"{key} is {value}")
+normal = "This is our company pillars"
+names = ["Mayur", "Deep", "Viraj", "Vir"]
+roles = {"Mayue" : "MD", "Deep" : "Marketing Head", "Viraj" : "Manufacturing", "Vir": "SDE"}
+funargs(*names, **roles)
+"""
+output:
+Mayur
+Deep
+Viraj
+Vir
+"""
+#output after normal and **kw argument
+"""
+output:
+This is our company pillars
+
+Deep
+Viraj
+Vir
+Every individual role:        
+
+Mayue is MD
+Deep is Marketing Head        
+Viraj is Manufacturing        
+Vir is SDE
+"""
