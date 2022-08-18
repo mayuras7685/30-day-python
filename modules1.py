7e1ef0b86fe56f9cc44eda924bb229272df95787
mayuras7685 <mayurasodara@gmail.com>
2022-08-18 20:49:12 +0530

    use external module

---
+++ modules1.py
@@ -0,0 +1,19 @@
+from random import random
+import random
+random_number = random.randint(0, 5)
+print(random_number)
+# random number in 0 to 5
+
+rand = random.random()*100
+print(rand)
+#output : 10.671142478829621
+
+lst = ["Star Plus", "DD1", "Colours", "SS1", "History TV18"]
+choice = random.choice(lst)
+print(choice)
+# random choice from lst 
+
+"""
+Download external module 
+pip install sklearn
+"""
