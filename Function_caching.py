056cd87d33e0ade41ba872b7c0b8d2bb1b76090e
mayuras7685 <mayurasodara@gmail.com>
2022-09-05 22:53:43 +0530

    function caching

---
+++ Function_caching.py
@@ -0,0 +1,17 @@
+import time
+from functools import lru_cache
+
+
+@lru_cache(maxsize=3)
+def some_work(n):
+    #some task taking n seconds
+    time.sleep(n)
+    return n
+
+
+if __name__ == '__main__':
+    print("Now running some work")
+    some_work(3)
+    print("done... calling again")
+    some_work(3)
+    print("called again")
