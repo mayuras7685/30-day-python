e56d415949947d2f84e7a72bc7534308de30a04b
mayuras7685 <mayurasodara@gmail.com>
2022-09-12 22:00:31 +0530

    multiple inheritances

--- OOPS/oops4.py
+++ OOPS/oops4.py
@@ -46,3 +46,39 @@ Not Mayur True
 
 # Multiple inheritances
 
+class Base1(object):
+    def __init__(self):
+        self.str1 = "abc"
+        print("Base1")
+ 
+ 
+class Base2(object):
+    def __init__(self):
+        self.str2 = "xyz"
+        print("Base2")
+ 
+ 
+class Derived(Base1, Base2):
+    def __init__(self):
+ 
+        # Calling constructors of Base1
+        # and Base2 classes
+        Base1.__init__(self)
+        Base2.__init__(self)
+        print("Derived")
+ 
+    def printStrs(self):
+        print(self.str1, self.str2)
+ 
+ 
+ob = Derived()
+ob.printStrs()
+
+"""
+output:
+Base1
+Base2
+Derived
+abc xyz
+"""
+
