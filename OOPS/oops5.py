52b6a0cb19e3c1eb25df5f23859ede95637ca8c6
mayuras7685 <mayurasodara@gmail.com>
2022-09-15 21:27:53 +0530

    encapsulation

---
+++ OOPS/oops5.py
@@ -0,0 +1,37 @@
+# Encapsulation
+"""Public,Private,Protected Access Specifier"""
+# public
+# protected
+# private
+
+class Employee:
+    no_of_leaves=0
+    public=4
+    _protect=90 # _ for protected var.
+    __private=100 # __ for private var.
+    def __init__(self,aname,asalery,arole):
+        self.name=aname
+        self.salery=asalery
+        self.role=arole
+
+    def printdetails(self):
+        return f"Employee name is {self.name}, salary is {self.salary} and role is {self.role}"
+
+    @classmethod
+    def change_leaves(cls, newleaves):
+        cls.no_of_leaves = newleaves
+
+    @classmethod
+    def from_str(cls, string):
+        # params=string.split("-")
+        # return cls(params[0],params[1],params[2])
+        return cls(*string.split("-"))
+
+    @staticmethod
+    def printgood(string):
+        print("this is good " + string)
+
+
+emp=Employee("mayur",345,"programmer")
+print(emp._protect)
+print(emp._Employee__private)
